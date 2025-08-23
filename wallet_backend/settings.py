84aa1a9c7140fa0719b94a23db2ab2f9c56570a1
Brijesh Yadav <brijeshkumar48@yahoo.com>
2025-08-23 19:28:49 +0530

    ✨ settings: update ALLOWED_HOSTS for production environment

--- wallet_backend/settings.py
+++ wallet_backend/settings.py
@@ -36,7 +36,12 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "wallet.thepmsquare.com",
+    "raspi.thepmsquare.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
